eecd8b7fc000f2a2cabf552e6f4827be3ea8bb7d
Conrad <a0099303@u.nus.edu>
2015-06-09 01:55:27 +0800

    Initial Commit

---
+++ Shader_Lib/Player.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shader_Lib
+{
+    public class Player
+    {
+        int[] _position;
+        int[] _dimensions;
+        int[,] _mapGrid;
+        List<int[]> objectCoordinates = new List<int[]>();
+
+        //Shader
+        int _viewRadius;
+        int[,] _shaderGrid;
+
+        public Player(int[,] mapGrid, int[] dimensions)
+        {
+            _mapGrid = mapGrid;
+            _position = new int[2]{0, 0};
+            _dimensions = dimensions; //ASSUMPTION: DIMENSIONS FOR SHADER AND MAP SAME
+            _viewRadius = dimensions[0] / 2; //ASSUMPTION: SAME X AND Y DIM
+            _shaderGrid = new int[dimensions[0], dimensions[1]];
+            InitializeGrid(_shaderGrid, dimensions, 0);
+
+        }
+
+        void InitializeGrid(int[,] array, int[] dimensions, int initVal)
+        {
+            int size = _dimensions.Count();
+            if (size == 2)
+            {
+                int xDimensions = _dimensions[0];
+                int yDimensions = _dimensions[1];
+
+                for (int i = 0; i < xDimensions; i++)
+                {
+                    for (int j = 0; j < yDimensions; j++)
+                    {
+                        array[i, j] = initVal;
+                    }
+                }
+            }
+        }
+
+        public void SetViewRadius(int radius)
+        {
+            _viewRadius = radius;
+        }
+
+        public int[,] GetShaderGrid()
+        {
+            RenderShaderGrid();
+            return _shaderGrid;
+            
+        }
+
+        void RenderShaderGrid()
+        {
+            RenderPlayerRadius();
+            RenderLineOfSight();
+
+        }
+        void RenderPlayerRadius()
+        {
+            
+            for (int curRadius = 0; curRadius < _viewRadius; curRadius++)
+            {
+                double angle = 0;
+                while (angle < (Math.PI * 2))
+                {
+                    int x_coordinate = (int)Math.Floor(curRadius * Math.Cos(angle)) + _dimensions[0] / 2;
+                    int y_coordinate = (int)Math.Floor(curRadius * Math.Sin(angle)) + _dimensions[1] / 2;
+                    angle = angle + (Math.PI * 2 / 1000);
+                    if (x_coordinate > 0 && y_coordinate > 0)
+                    {
+                        if (x_coordinate < _dimensions[0] && y_coordinate < _dimensions[1])
+                        {
+                            _shaderGrid[x_coordinate, y_coordinate] = 1;
+                        }
+                    }
+
+                }
+            }
+        }
+        void RenderLineOfSight()
+        {
+            int[] objectTestCoord = new int[2]{10, 10};
+            int[] lineVector = GetVector(_position, objectTestCoord);
+            for (int i = 0; i < _viewRadius; i++)
+            {
+                //INCOMPLETE
+            }
+
+        }
+
+        int[] GetVector(int[] pointA, int[] pointB)
+        {
+            int dim = pointA.Count();
+            int[] output = new int[dim];
+            
+            if (pointA.Count() == pointB.Count())
+            {
+                for (int i = 0; i < dim; i++)
+                {
+                    output[i] = pointB[i] - pointA[i];
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Shader_Testdriver/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shader_Lib;
+namespace Shader_Testdriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = new int[2];
+            dimensions[0] = 30;
+            dimensions[1] = 30;
+            int[,] mapGrid = new int[dimensions[0], dimensions[1]];
+            
+            InitializeGrid(ref mapGrid, dimensions, 1);
+            //PrintGrid(mapGrid, dimensions);
+            int[] treePos = new int[2]{1, 2};
+            SetObjectPosition(ref mapGrid, treePos, dimensions, 7);
+            Player player = new Player(mapGrid, dimensions);
+            int radius = 0;
+            while (radius < 100)
+            {
+                player.SetViewRadius(radius);
+                ++radius;
+                int[,] playerView = player.GetShaderGrid();
+                PrintGrid(playerView, dimensions);
+                System.Threading.Thread.Sleep(500);
+                
+            }
+            Console.ReadKey();
+        }
+
+        static void InitializeGrid(ref int[,] array, int[] dimensions, int initVal)
+        {
+            int size = dimensions.Count();
+            if (size == 2)
+            {
+                int xDimensions = dimensions[0];
+                int yDimensions = dimensions[1];
+
+                for (int i = 0; i < xDimensions; i++)
+                {
+                    for (int j = 0; j < yDimensions; j++)
+                    {
+                        array[i, j] = initVal;
+                    }
+                }
+            }
+        }
+
+        static void PrintGrid(int[,] array, int[] dimensions){
+           int size = dimensions.Count();
+            if (size == 2)
+            {
+                int xDimensions = dimensions[0];
+                int yDimensions = dimensions[1];
+
+                for (int i = 0; i < xDimensions; i++)
+                {
+                    string line = "";
+                    for (int j = 0; j < yDimensions; j++)
+                    {
+                        line += array[i, j];
+                        line += " ";
+                    }
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+        static void SetObjectPosition(ref int[,] mapGrid, int[] position, int[] dimensions, int objectValue)
+        {
+            if (position.Count() == 2 && dimensions.Count() == 2)
+            {
+                if (position[0] > 0 && position[1] > 0)
+                {
+                    if (position[0] < dimensions[0] && position[1] < dimensions[1])
+                    {
+                        int x = position[0];
+                        int y = position[1];
+                        
+                        mapGrid[x,y] = objectValue;
+                    }
+                }
+            }
+        }
+    }
+}
